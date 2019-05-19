0639078a849c7844eee24e6e18ea117c7811808b
Eden Szeto <szeto236@yahoo.com.hk>
2019-05-19 22:57:29 +0800

    refactor: add empty star icon and tier data

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import useSheetApi from "./hooks/useSheetApi";
 import Character from "./components/Character";
 import { GlobalStyles } from "./components/GlobalStyles";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
-import styled from "styled-components";
+import { faStar as fasFaStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as farFaStar } from "@fortawesome/free-regular-svg-icons";
+import styled from "styled-components/macro";
 
-library.add(faStar);
+library.add(fasFaStar, farFaStar);
 
 const GridWrapper = styled.div`
   display: grid;
@@ -14,33 +15,36 @@ const GridWrapper = styled.div`
   grid-template-rows: auto;
 `;
 
-const App: React.FC = () => {
-  const params = {
-    spreadsheetId: "1XoNQlKdUsCMQ0Ew6g5q_nqQuJMccv8XrAbkDypJQdqI",
-    sheetRange: "Roster!A3:W102",
-    apiKey: process.env.REACT_APP_API_KEY as string,
-    majorDimension: "ROWS"
-  };
-  const data = useSheetApi({ ...params });
+const params = {
+  spreadsheetId: "1XoNQlKdUsCMQ0Ew6g5q_nqQuJMccv8XrAbkDypJQdqI",
+  sheetRange: "Roster!A3:W102",
+  apiKey: process.env.REACT_APP_API_KEY as string,
+  majorDimension: "ROWS"
+};
 
+const App: React.FC = () => {
+  const data = useSheetApi({ ...params });
   return (
-    <GridWrapper>
+    <React.Fragment>
       <GlobalStyles />
-      {data.values.map(char => {
-        const imageUrl = `${process.env.PUBLIC_URL}/images/${char[1]}.png`;
-        return (
-          <Character
-            key={char[2]}
-            imageUrl={imageUrl}
-            charName={char[2]}
-            star={char[12]}
-            rStar={char[13]}
-            level={char[14]}
-            power={char[20]}
-          />
-        );
-      })}
-    </GridWrapper>
+      <GridWrapper>
+        {data.values.map(char => {
+          const imageUrl = `${process.env.PUBLIC_URL}/images/${char[1]}.png`;
+          return (
+            <Character
+              key={char[2]}
+              imageUrl={imageUrl}
+              charName={char[2]}
+              star={char[12]}
+              rStar={char[13]}
+              level={char[14]}
+              tier={char[19]}
+              power={char[20]}
+            />
+          );
+        })}
+      </GridWrapper>
+    </React.Fragment>
   );
 };
 
