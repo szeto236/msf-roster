7258830b74f02b617ee4052c7b1b949210485066
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-26 13:51:13 +0000

    Deploy szeto236/msf-roster to github.com/szeto236/msf-roster.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/msf-roster/precache-manifest.8e049da2f4ac85e1cecc9a68cd6f5952.js"
+  "/msf-roster/precache-manifest.ef28ad56da0fd2a9df89a8451b722621.js"
 );
 
 self.addEventListener('message', (event) => {
