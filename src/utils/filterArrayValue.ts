09ec1b9779cd00d19ad913e5b043a839c37256e9
Eden Szeto <szeto236@yahoo.com.hk>
2019-05-26 21:47:01 +0800

    feat: add filter array values by array utils

--- src/utils/filterArrayValue.ts
+++ src/utils/filterArrayValue.ts
@@ -1,4 +1,9 @@
 import * as R from "ramda";
 
-export const filterArrayValue = (data: any[][], val: string): any[][] =>
-  R.filter(R.where([R.contains(val)]), data);
+export const filterArrayValue = (
+  data: any[][],
+  val: string | string[]
+): any[][] => R.filter(R.where([R.contains(`${val}`)]), data);
+
+export const filterArrayValueByArray = (data: any[][], arr: string[]) =>
+  R.map(val => R.flatten(filterArrayValue(data, val)), arr);
